2265b5e9a09827a705980f2da2f26eca11301c47
Charles Woodruff <cwoodruff@rithmicmedia.com>
2023-04-23 14:31:31 -0400

    Added MudBlazor and layout.

--- RithmicSoul.Admin/Client/Program.cs
+++ RithmicSoul.Admin/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using RithmicSoul.Admin.Client;
 
 namespace RithmicSoul.Admin.Client
@@ -13,7 +14,7 @@ namespace RithmicSoul.Admin.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
     }
