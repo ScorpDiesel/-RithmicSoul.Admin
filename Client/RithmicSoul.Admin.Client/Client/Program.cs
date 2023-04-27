51cf467126b28fba3bbe2fb64d4940f33e57d5ef
Charles Woodruff <cwoodruff@rithmicmedia.com>
2023-04-26 21:58:29 -0400

    Replaced models with dtos.  Added icons to the QuestionType pane.  Used ObservableCollection instead of IEnumerable so when items are added or deleted the data source list, the data grid should update.

--- Client/RithmicSoul.Admin.Client/Client/Program.cs
+++ Client/RithmicSoul.Admin.Client/Client/Program.cs
@@ -13,7 +13,14 @@ namespace RithmicSoul.Admin.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            if (builder.HostEnvironment.IsDevelopment())
+            {
+                builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7129/api/") });
+            }
+            else
+            {
+                builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            }
 
             builder.Services.AddMudServices();
             await builder.Build().RunAsync();
