7e412d2df652b3bb035c13d29a4883ad1ae2ed98
Charles Woodruff <cwoodruff@rithmicmedia.com>
2023-06-08 18:47:07 -0400

    Called an endpoint that sends back byte data for the image and audio. Created svg spinner loading image.

--- Client/RithmicSoul.Admin.Client/Pages/Surveys/AdinkraSurvey.razor.cs
+++ Client/RithmicSoul.Admin.Client/Pages/Surveys/AdinkraSurvey.razor.cs
@@ -6,36 +6,42 @@ namespace RithmicSoul.Admin.Client.Pages.Surveys;
 
 public partial class AdinkraSurvey : ComponentBase
 {
+    [Inject] private HttpClient _httpClient { get; set; }
     [Inject] private IService<AdinkraSymbolDto> AdinkraSymbolService { get; set; }
 
     private IEnumerable<AdinkraSymbolDto>? _allSymbols;
-    private IEnumerable<AdinkraSymbolDto>? CurrentSymbols => _allSymbols?.Skip(_currentPage * _pageSize).Take(_pageSize);
+    private IEnumerable<AdinkraSymbolDto>? CurrentSymbols => _allSymbols?.Skip(_currentPage * _pageSize).Take(_pageSize); [CascadingParameter]
+    public EventCallback HideMenus { get; set; }
 
     private int _pageSize = 10;
-    private int _currentPage = 0;
+    private int _currentPage;
     private HashSet<int> _symbolLikes = new();
+    private string? _baseAddress;
 
     protected override async Task OnInitializedAsync()
     {
+        await HideMenus.InvokeAsync();
+        _baseAddress = _httpClient.BaseAddress?.ToString();
         _allSymbols = await AdinkraSymbolService.GetAllAsync();
     }
 
     private bool HasPreviousPage => _currentPage > 0;
     private bool HasNextPage => (_currentPage + 1) * _pageSize < _allSymbols?.Count();
 
-    private void PreviousPage()
+    private void PreviousPageAsync()
     {
-        if (HasPreviousPage) _currentPage--;
+        if (!HasPreviousPage) return;
+        _currentPage--;
     }
 
-    private void NextPage()
+    private void NextPageAsync()
     {
-        if (HasNextPage) _currentPage++;
+        if (!HasNextPage) return;
+        _currentPage++;
     }
 
     public void TallySymbolLikes(bool isSelected, int symbolId)
     {
-        Console.WriteLine(isSelected);
         if (isSelected)
         {
             _symbolLikes.Add(symbolId);
--- Client/RithmicSoul.Admin.Client/Views/Components/AdinkraSymbolCard.razor.cs
+++ Client/RithmicSoul.Admin.Client/Views/Components/AdinkraSymbolCard.razor.cs
@@ -1,31 +1,70 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
+using RithmicSoul.Admin.Client.Pages.Surveys;
 
 namespace RithmicSoul.Admin.Client.Views.Components;
 
 public partial class AdinkraSymbolCard : ComponentBase
 {
-    [Parameter] public string ImgSrc { get; set; }
-    [Parameter] public string AudioSrc { get; set; }
+    [Parameter] public int SymbolId { get; set; }
     [Parameter] public string SymbolName { get; set; }
     [Parameter] public string SymbolTranslation { get; set; }
     [Parameter] public string SymbolMeaning { get; set; }
-    [Parameter] public int SymbolId { get; set; }
-    [Parameter] public HashSet<int> SymbolLikes { get; set; }
+    [Parameter] public string ImgSrcUrl { get; set; }
+    [Parameter] public string AudioSrcUrl { get; set; }
+    [Parameter] public string Style { get; set; }
     [Parameter] public EventCallback<bool> OnSymbolClicked { get; set; }
-    
-    private bool _selected;
+    [Parameter] public bool IsSelected { get; set; }
+
+    private int _maxMeaningLength = 50;
+    private int _maxTranslationLength = 35;
+    private int _maxNameLength = 25;
+    private int _elevation = 1;
+    private bool _isLoading = true;
+    private string _contentStyle;
+    private string _loadingStyle;
+
+    protected override void OnInitialized()
+    {
+        _contentStyle = "display: none;";
+        _loadingStyle = "";
+    }
 
     protected override void OnAfterRender(bool firstRender)
     {
-        _selected = SymbolLikes.Contains(SymbolId);
+        _contentStyle = "display: none;";
+        _loadingStyle = "";
     }
 
     public async Task SymbolClickedAsync()
     {
-        _selected = !_selected;
-        await OnSymbolClicked.InvokeAsync(_selected);
+        IsSelected = !IsSelected;
+        await OnSymbolClicked.InvokeAsync(IsSelected);
     }
 
-    private Color IconButtonColor => _selected ? Color.Error : Color.Default;
+    private Color IconButtonColor => IsSelected ? Color.Error : Color.Default;
+
+    private bool IsStringLengthTooLong(string str, int maxLength)
+    {
+        return str.Length > maxLength;
+    }
+
+    private void CardElevationUp()
+    {
+        DisplayContent(null);
+        _elevation = 7;
+    }
+
+    private void CardElevationDown()
+    {
+        DisplayContent(null);
+        _elevation = 1;
+    }
+
+    private void DisplayContent(EventArgs obj)
+    {
+        _contentStyle = "";
+        _loadingStyle = "display: none;";
+    }
 }
