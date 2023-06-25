36f92682bc16ec53fdc240acd828da05a02f29a7
Charles Woodruff <cwoodruff@rithmicmedia.com>
2023-06-25 13:19:07 -0400

    ISurveyResponseService was added.

---
+++ RithmicSoul.Admin.Application/Interfaces/Services/Survey/ISurveyResponseService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using RithmicSoul.Models.Survey.Dtos;
+
+namespace RithmicSoul.Admin.Application.Interfaces.Services.Survey;
+
+public interface ISurveyResponseService
+{
+    Task<IEnumerable<SurveyResponseDto>> GetAllResponsesAsync();
+    Task<IEnumerable<SurveyResponseDto>> GetResponsesByIdAsync(Guid id);
+    Task<IEnumerable<SurveyResponseDto>> GetResponsesAsync(Expression<Func<SurveyResponseDto, bool>> expression);
+    Task<bool> InsertResponsesAsync(SurveyResponseDto dto);
+    Task<bool> UpdateResponsesAsync(SurveyResponseDto dto);
+    Task<bool> DeleteResponsesAsync(SurveyResponseDto dto);
+}
