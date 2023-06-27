48a944d4376d844f2da7a0d233563e8360f3de49
Charles Woodruff <cwoodruff@rithmicmedia.com>
2023-06-27 18:25:19 -0400

    Created RsRankCard.razor.

---
+++ Client/RithmicSoul.Admin.Client/Views/Components/RsRankCard.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace RithmicSoul.Admin.Client.Views.Components;
+
+public partial class RsRankCard : ComponentBase
+{
+    [Parameter] public string RankListHeader { get; set; }
+    [Parameter] public List<string> Items { get; set; }
+
+    private string _rankDropZone = "RankDropZone";
+
+    private void ItemUpdated(MudItemDropInfo<string> dropItem)
+    {
+        //dropItem.Item.Selector = dropItem.DropzoneIdentifier;
+    }
+}
